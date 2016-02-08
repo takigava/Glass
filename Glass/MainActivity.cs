44c8b253c7cc8e038925d8812bc79f3cd7c40d64
takigava <tired_heart@mail.ru>
2016-02-08 17:01:54 +0300

    Initial commit

---
+++ Glass/MainActivity.cs
@@ -0,0 +1,157 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using GlassExtensions;
+using Android.Views;
+using Java.Util;
+using Android.Animation;
+using Android.Views.Animations;
+using System;
+using Android.Webkit;
+using Android.Util;
+
+namespace Glass
+{
+	[Activity (Label = "Glass", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		private BlurringView mBlurringView;
+
+		private int[] mImageIds = {
+			Resource.Drawable.p0, Resource.Drawable.p1, Resource.Drawable.p2, Resource.Drawable.p3, Resource.Drawable.p4,
+			Resource.Drawable.p5, Resource.Drawable.p6, Resource.Drawable.p7, Resource.Drawable.p8, Resource.Drawable.p9
+		};
+
+		private ImageView[] mImageViews = new ImageView[9];
+		private int mStartIndex;
+
+		private Java.Util.Random mRandom = new Java.Util.Random();
+
+		private bool mShifted;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.activity_main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			mBlurringView = (BlurringView) FindViewById(Resource.Id.blurring_view);
+			View blurredView = FindViewById(Resource.Id.blurred_view);
+
+			// Give the blurring view a reference to the blurred view.
+			mBlurringView.setBlurredView(blurredView);
+
+			mImageViews[0] = (ImageView) FindViewById(Resource.Id.image0);
+			mImageViews[1] = (ImageView) FindViewById(Resource.Id.image1);
+			mImageViews[2] = (ImageView) FindViewById(Resource.Id.image2);
+			mImageViews[3] = (ImageView) FindViewById(Resource.Id.image3);
+			mImageViews[4] = (ImageView) FindViewById(Resource.Id.image4);
+			mImageViews[5] = (ImageView) FindViewById(Resource.Id.image5);
+			mImageViews[6] = (ImageView) FindViewById(Resource.Id.image6);
+			mImageViews[7] = (ImageView) FindViewById(Resource.Id.image7);
+			mImageViews[8] = (ImageView) FindViewById(Resource.Id.image8);
+
+			DisplayMetrics metrics = new DisplayMetrics();
+
+			WindowManager.DefaultDisplay.GetMetrics(metrics);
+
+			double inches = Math.Sqrt((metrics.WidthPixels * metrics.WidthPixels) + (metrics.HeightPixels * metrics.HeightPixels)) / metrics.Density;
+		}
+
+		[Java.Interop.Export("shuffle")]
+		public void shuffle(View view) {
+
+			// Randomly pick a different start in the array of available images.
+			int newStartIndex;
+			do {
+				newStartIndex = mImageIds[mRandom.NextInt(mImageIds.Length)];
+			} while (newStartIndex == mStartIndex);
+			mStartIndex = newStartIndex;
+
+			// Update the images for the image views contained in the blurred view.
+			for (int i = 0; i < mImageViews.Length; i++) {
+				int drawableId = mImageIds[(mStartIndex + i) % mImageIds.Length];
+				mImageViews[i].SetImageDrawable(Resources.GetDrawable(drawableId));
+			}
+
+			// Invalidates the blurring view when the content of the blurred view changes.
+			mBlurringView.Invalidate();
+		}
+
+		[Java.Interop.Export("shift")]
+		public void shift(View view) {
+			if (!mShifted) {
+				foreach (ImageView imageView in mImageViews) {
+					ObjectAnimator tx = ObjectAnimator.OfFloat(imageView, View.X, (mRandom.NextFloat() - 0.5f) * 500);
+					tx.AddUpdateListener(new AnimListener(mBlurringView));
+					ObjectAnimator ty = ObjectAnimator.OfFloat(imageView, View.Y, (mRandom.NextFloat() - 0.5f) * 500);
+					ty.AddUpdateListener(new AnimListener(mBlurringView));
+					AnimatorSet set = new AnimatorSet();
+					set.PlayTogether(tx, ty);
+					set.SetDuration(3000);
+					set.SetInterpolator(new OvershootInterpolator());
+					set.AddListener(new AnimationEndListener(imageView));
+					set.Start();
+				};
+				mShifted = true;
+			} else {
+				foreach (ImageView imageView in mImageViews) {
+					ObjectAnimator tx = ObjectAnimator.OfFloat(imageView, View.X, 0);
+					tx.AddUpdateListener(new AnimListener(mBlurringView));
+					ObjectAnimator ty = ObjectAnimator.OfFloat(imageView, View.Y, 0);
+					ty.AddUpdateListener(new AnimListener(mBlurringView));
+					AnimatorSet set = new AnimatorSet();
+					set.PlayTogether(tx, ty);
+					set.SetDuration(3000);
+					set.SetInterpolator(new OvershootInterpolator());
+					set.AddListener(new AnimationEndListener(imageView));
+					set.Start();
+				};
+				mShifted = false;
+			}
+		}
+	}
+
+	public class AnimationEndListener : Java.Lang.Object, Animator.IAnimatorListener {
+
+		View mView;
+
+		public AnimationEndListener(View v) {
+			mView = v;
+		}
+			
+		public void OnAnimationStart(Animator animation) {
+			mView.SetLayerType(LayerType.Hardware, null);
+		}
+			
+		public void OnAnimationEnd(Animator animation) {
+			mView.SetLayerType(LayerType.None, null);
+		}
+			
+		public void OnAnimationCancel(Animator animation) {
+			mView.SetLayerType(LayerType.None, null);
+		}
+			
+		public void OnAnimationRepeat(Animator animation) {
+
+		}
+	}
+
+	public class AnimListener : Java.Lang.Object, ValueAnimator.IAnimatorUpdateListener
+	{
+		BlurringView mBlurringView;
+
+		public AnimListener(BlurringView v) {
+			mBlurringView = v;
+		}
+
+		public void OnAnimationUpdate(ValueAnimator animation) {
+			mBlurringView.Invalidate();
+		}
+	}
+}
+
+
---
+++ GlassExtensions/Blur/BlurringView.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Android.Views;
+using Android.Graphics;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Util;
+//using Android.Support.V8.Renderscript;
+using Android.Renderscripts;
+
+namespace GlassExtensions
+{
+	public class BlurringView : View
+	{
+		private int mDownsampleFactor;
+		private Color mOverlayColor;
+
+		private View mBlurredView;
+		private int mBlurredViewWidth, mBlurredViewHeight;
+
+		private bool mDownsampleFactorChanged;
+		private Bitmap mBitmapToBlur, mBlurredBitmap;
+		private Canvas mBlurringCanvas;
+		private RenderScript mRenderScript;
+		private ScriptIntrinsicBlur mBlurScript;
+		private Allocation mBlurInput, mBlurOutput;
+
+		public BlurringView(Context context):base(context, null) {
+		}
+
+		public BlurringView(Context context, IAttributeSet attrs) :base(context, attrs){
+			
+
+			Resources res = Resources;
+			int defaultBlurRadius = res.GetInteger(Resource.Integer.default_blur_radius);
+			int defaultDownsampleFactor = res.GetInteger(Resource.Integer.default_downsample_factor);
+			Color defaultOverlayColor = res.GetColor(Resource.Color.default_overlay_color);
+
+			initializeRenderScript(context);
+
+			TypedArray a = context.ObtainStyledAttributes(attrs, Resource.Styleable.PxBlurringView);
+			setBlurRadius(a.GetInt(Resource.Styleable.PxBlurringView_blurRadius, defaultBlurRadius));
+			setDownsampleFactor(a.GetInt(Resource.Styleable.PxBlurringView_downsampleFactor,
+				defaultDownsampleFactor));
+			setOverlayColor(a.GetColor(Resource.Styleable.PxBlurringView_overlayColor, defaultOverlayColor));
+			a.Recycle();
+		}
+
+		public void setBlurredView(View blurredView) {
+			mBlurredView = blurredView;
+		}
+			
+		protected override void OnDraw(Canvas canvas) {
+			base.OnDraw (canvas);
+			if (mBlurredView != null) {
+				if (prepare()) {
+					// If the background of the blurred view is a color drawable, we use it to clear
+					// the blurring canvas, which ensures that edges of the child views are blurred
+					// as well; otherwise we clear the blurring canvas with a transparent color.
+					if (mBlurredView.Background != null && mBlurredView.Background is ColorDrawable){
+						mBitmapToBlur.EraseColor(((ColorDrawable) mBlurredView.Background).Color);
+					}else {
+						mBitmapToBlur.EraseColor(Color.Transparent);
+					}
+
+					mBlurredView.Draw(mBlurringCanvas);
+					blur();
+
+					canvas.Save();
+					canvas.Translate(mBlurredView.GetX() - GetX(), mBlurredView.GetY() - GetY());
+					canvas.Scale(mDownsampleFactor, mDownsampleFactor);
+					canvas.DrawBitmap(mBlurredBitmap, 0, 0, null);
+					canvas.Restore();
+				}
+				canvas.DrawColor(mOverlayColor);
+			}
+		}
+
+		public void setBlurRadius(int radius) {
+			mBlurScript.SetRadius(radius);
+		}
+
+		public void setDownsampleFactor(int factor) {
+			if (factor <= 0) {
+				throw new RSIllegalArgumentException("Downsample factor must be greater than 0.");
+			}
+
+			if (mDownsampleFactor != factor) {
+				mDownsampleFactor = factor;
+				mDownsampleFactorChanged = true;
+			}
+		}
+
+		public void setOverlayColor(Color color) {
+			mOverlayColor = color;
+		}
+
+		private void initializeRenderScript(Context context) {
+			mRenderScript = RenderScript.Create(context);
+			mBlurScript = ScriptIntrinsicBlur.Create(mRenderScript, Element.U8_4(mRenderScript));
+		}
+
+		protected bool prepare() {
+			int width = mBlurredView.Width;
+			int height = mBlurredView.Height;
+
+			if (mBlurringCanvas == null || mDownsampleFactorChanged
+				|| mBlurredViewWidth != width || mBlurredViewHeight != height) {
+				mDownsampleFactorChanged = false;
+
+				mBlurredViewWidth = width;
+				mBlurredViewHeight = height;
+
+				int scaledWidth = width / mDownsampleFactor;
+				int scaledHeight = height / mDownsampleFactor;
+
+				// The following manipulation is to avoid some RenderScript artifacts at the edge.
+				scaledWidth = scaledWidth - scaledWidth % 4 + 4;
+				scaledHeight = scaledHeight - scaledHeight % 4 + 4;
+
+				if (mBlurredBitmap == null
+					|| mBlurredBitmap.Width != scaledWidth
+					|| mBlurredBitmap.Height != scaledHeight) {
+					mBitmapToBlur = Bitmap.CreateBitmap(scaledWidth, scaledHeight,
+						Bitmap.Config.Argb8888);
+					if (mBitmapToBlur == null) {
+						return false;
+					}
+
+					mBlurredBitmap = Bitmap.CreateBitmap(scaledWidth, scaledHeight,
+						Bitmap.Config.Argb8888);
+					if (mBlurredBitmap == null) {
+						return false;
+					}
+				}
+
+				mBlurringCanvas = new Canvas(mBitmapToBlur);
+				mBlurringCanvas.Scale(1f / mDownsampleFactor, 1f / mDownsampleFactor);
+				mBlurInput = Allocation.CreateFromBitmap(mRenderScript, mBitmapToBlur,
+					Allocation.MipmapControl.MipmapNone,AllocationUsage.Script);
+				mBlurOutput = Allocation.CreateTyped(mRenderScript, mBlurInput.Type);
+			}
+			return true;
+		}
+
+		protected void blur() {
+			mBlurInput.CopyFrom(mBitmapToBlur);
+			mBlurScript.SetInput(mBlurInput);
+			mBlurScript.ForEach(mBlurOutput);
+			mBlurOutput.CopyTo(mBlurredBitmap);
+		}
+			
+		protected override void OnDetachedFromWindow() {
+			base.OnDetachedFromWindow();
+			if (mRenderScript != null){
+				mRenderScript.Destroy();
+			}
+		}
+	}
+}
+
